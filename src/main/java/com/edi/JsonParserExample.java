939a494c5c92149a8df7ed2d69ee70d3c94a7bee
Keerthi Kongara <keerthi@digitalhie.com>
2024-01-19 09:01:42 -0500

    Added parser code

---
+++ src/main/java/com/edi/JsonParserExample.java
@@ -0,0 +1,31 @@
+package com.edi;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class JsonParserExample {
+    public static void main(String[] args) {
+        String jsonString = "{\"name\":\"John\", \"age\":30}";
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            Person person = mapper.readValue(jsonString, Person.class);
+            System.out.println("Name: " + person.getName() + ", Age: " + person.getAge());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class Person {
+        private String name;
+        private int age;
+
+        // Getters and setters
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+        public int getAge() { return age; }
+        public void setAge(int age) { this.age = age; }
+    }
+}
+
